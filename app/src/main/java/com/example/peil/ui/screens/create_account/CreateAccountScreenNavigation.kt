1394d8572e294d4ce712523c6d2401c8f92ee423
NymiKo <dimon.kabernik8@gmail.com>
2023-12-23 23:45:30 +0500

    Methods for navigation in the CreateAccountScreen have been moved to a separate file

---
+++ app/src/main/java/com/example/peil/ui/screens/create_account/CreateAccountScreenNavigation.kt
@@ -0,0 +1,38 @@
+package com.example.peil.ui.screens.create_account
+
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+private const val emailKeyArg = "email"
+private const val createAcountScreen = "create_account"
+private const val createAcountScreenRoute = "create_account/{$emailKeyArg}"
+
+fun NavGraphBuilder.createAccountScreen(onRegistrationClick: () -> Unit, showHaveAccountDialog: () -> Unit, onBack: () -> Unit) {
+    composable(
+        route = createAcountScreenRoute,
+        arguments = listOf(
+            navArgument(emailKeyArg) { type = NavType.StringType }
+        )
+    ) {backStackEntry ->
+        val email = backStackEntry.arguments?.getString(emailKeyArg)
+
+        val viewModel = hiltViewModel<CreateAccountViewModel>()
+        CreateAccountScreen(
+            onRegistrationClick = onRegistrationClick::invoke,
+            showHaveAccountDialog = showHaveAccountDialog,
+            onBack = onBack::invoke,
+            viewModel = viewModel,
+            email = email
+        )
+    }
+}
+
+fun NavController.navigateToCreateAccountScreen(email: String) {
+    if (email.isNotEmpty()) {
+        navigate("$createAcountScreen/$email")
+    }
+}
