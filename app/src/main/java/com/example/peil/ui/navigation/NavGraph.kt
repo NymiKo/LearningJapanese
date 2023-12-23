a0d396881925dcdc6fe3485b2870d1f0ef42570c
NymiKo <dimon.kabernik8@gmail.com>
2023-12-23 23:46:25 +0500

    Navigation in NavGraph has been changed

--- app/src/main/java/com/example/peil/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/peil/ui/navigation/NavGraph.kt
@@ -1,19 +1,16 @@
 package com.example.peil.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import com.example.peil.ui.screens.create_account.CreateAccountScreen
-import com.example.peil.ui.screens.create_account.CreateAccountViewModel
-import com.example.peil.ui.screens.login.LoginScreen
+import com.example.peil.ui.screens.create_account.createAccountScreen
+import com.example.peil.ui.screens.create_account.dialog_screen.haveAccountDialog
+import com.example.peil.ui.screens.create_account.dialog_screen.navigateToHaveAccountDialog
+import com.example.peil.ui.screens.create_account.navigateToCreateAccountScreen
 import com.example.peil.ui.screens.login.loginScreen
 import com.example.peil.ui.screens.login.navigateToLoginScreen
-import com.example.peil.ui.screens.registration.RegistrationEmailScreen
-import com.example.peil.ui.screens.registration.RegistrationEmailViewModel
-import com.example.peil.ui.screens.welcome.WelcomeScreen
+import com.example.peil.ui.screens.registration.navigateToRegistrationEmailScreen
+import com.example.peil.ui.screens.registration.registrationScreen
 import com.example.peil.ui.screens.welcome.popBackStackToWelcomeScreen
 import com.example.peil.ui.screens.welcome.welcomeScreen
 
@@ -23,22 +20,28 @@ fun NavGraph(navController: NavHostController) {
         navController = navController,
         startDestination = Screens.Welcome.route
     ) {
-        welcomeScreen(onLoginClick = navController::navigateToLoginScreen)
+        welcomeScreen(
+            onLoginClick = navController::navigateToLoginScreen,
+            onRegistrationEmailClick = navController::navigateToRegistrationEmailScreen
+        )
 
         loginScreen({}, onBack = navController::popBackStackToWelcomeScreen)
 
-        composable(route = Screens.RegistrationEmail.route) {
-            val viewModel = hiltViewModel<RegistrationEmailViewModel>()
-            RegistrationEmailScreen(navController, viewModel)
-        }
+        registrationScreen(
+            onCreateAccountClick = { email -> navController.navigateToCreateAccountScreen(email) },
+            onLoginClick = navController::navigateToLoginScreen,
+            onBack = navController::popBackStack
+        )
 
-        composable(route = Screens.CreateAccount.route + "/{email}") { backStackEntry ->
-            val viewModel = hiltViewModel<CreateAccountViewModel>()
-            CreateAccountScreen(
-                navController,
-                viewModel,
-                backStackEntry.arguments?.getString("email")
-            )
-        }
+        createAccountScreen(
+            {},
+            showHaveAccountDialog = navController::navigateToHaveAccountDialog,
+            onBack = navController::popBackStack
+        )
+
+        haveAccountDialog(
+            onLoginClick = navController::navigateToLoginScreen,
+            onDismissRequest = navController::popBackStack
+        )
     }
 }
--- app/src/main/java/com/example/peil/ui/screens/welcome/WelcomeScreenNavigation.kt
+++ app/src/main/java/com/example/peil/ui/screens/welcome/WelcomeScreenNavigation.kt
@@ -6,16 +6,12 @@ import androidx.navigation.compose.composable
 
 private const val welcomeScreenRoute = "welcome_screen"
 
-fun NavGraphBuilder.welcomeScreen(onLoginClick: () -> Unit) {
+fun NavGraphBuilder.welcomeScreen(onLoginClick: () -> Unit, onRegistrationEmailClick: () -> Unit) {
     composable(welcomeScreenRoute) {
-        WelcomeScreen(onLoginClick::invoke)
+        WelcomeScreen(onLoginClick = onLoginClick::invoke, onRegistrationEmailClick = onRegistrationEmailClick::invoke)
     }
 }
 
 fun NavController.popBackStackToWelcomeScreen() {
     popBackStack(route = welcomeScreenRoute, inclusive = false)
-}
-
-fun NavController.navigateToWelcomeScreen() {
-    navigate(welcomeScreenRoute)
 }
