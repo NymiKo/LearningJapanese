5d9960612f51b3f9d13bf2215ae8012bab7f7399
NymiKo <dimon.kabernik8@gmail.com>
2024-01-04 21:10:16 +0500

    The UI for the lesson list has been changed

--- app/src/main/java/com/example/peil/ui/screens/lessons_list/LessonsListScreen.kt
+++ app/src/main/java/com/example/peil/ui/screens/lessons_list/LessonsListScreen.kt
@@ -1,15 +1,13 @@
 package com.example.peil.ui.screens.lessons_list
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,41 +16,39 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CloudDownload
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.peil.R
+import com.example.peil.ui.theme.Blue
 import com.example.peil.ui.theme.Green
-import com.example.peil.ui.theme.GreenLight
 import com.example.peil.ui.theme.GreyLight
 import com.example.peil.ui.theme.White
 
@@ -146,54 +142,82 @@ private fun LessonsListComponent(
 ) {
     LazyColumn(
         modifier = modifier,
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        contentPadding = PaddingValues(horizontal = 16.dp)
     ) {
         items(10) {
-            LessonItemContainer()
+            LessonItem(lastItem = it == 9)
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
-private fun LessonItemContainer(
+private fun LessonItem(
     modifier: Modifier = Modifier,
+    lastItem: Boolean
 ) {
-    var expandedState by remember { mutableStateOf(false) }
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .animateContentSize(
-                animationSpec = tween(durationMillis = 300, easing = LinearOutSlowInEasing)
-            ),
-        shape = RoundedCornerShape(8.dp),
-        onClick = {
-            expandedState = !expandedState
-        }
+    Row(
+        modifier = modifier,
     ) {
-        LessonItemContent(expandedState = expandedState)
+        ImageLessonAndDivider(lastItem = lastItem)
+
+        Row(
+            modifier = Modifier
+                .height(74.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = "Урок 1",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Icon(
+                modifier = Modifier.size(20.dp),
+                imageVector = Icons.Default.CloudDownload,
+                contentDescription = stringResource(id = R.string.download_lesson),
+                tint = Blue
+            )
+        }
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-private fun LessonItemContent(modifier: Modifier = Modifier, expandedState: Boolean) {
+fun ImageLessonAndDivider(modifier: Modifier = Modifier, lastItem: Boolean) {
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Lesson Name", fontWeight = FontWeight.Bold)
-        Text(text = "Lesson time", fontSize = 14.sp)
-        if (expandedState) {
-            Card(
+        Box(
+            modifier = Modifier
+                .padding(6.dp)
+                .size(70.dp)
+                .border(4.dp, GreyLight, CircleShape)
+                .padding(4.dp)
+        ) {
+            GlideImage(
+                model = "https://www.mindtools.com/wp-content/uploads/legacy/972/GI_544466394_stockstudioX.jpg",
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(70.dp)
-                    .padding(top = 8.dp),
-                colors = CardDefaults.cardColors(containerColor = Color.Black)
-            ) {
-                Text(text = "Image")
-            }
+                    .fillMaxSize()
+                    .border(4.dp, White, CircleShape)
+                    .clip(CircleShape)
+            )
+        }
+
+        if(!lastItem) {
+            Divider(
+                color = GreyLight,
+                modifier = Modifier
+                    .height(20.dp)
+                    .width(3.dp)
+            )
         }
     }
 }
