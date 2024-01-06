352bdf0a5f927f92494c4f810a6de8bcbbf3c1fc
NymiKo <dimon.kabernik8@gmail.com>
2024-01-06 21:01:02 +0500

    Changed the execution type for ui recomposition when changing

--- app/src/main/java/com/example/peil/ui/screens/learning_lesson/data/model/SubLessonModel.kt
+++ app/src/main/java/com/example/peil/ui/screens/learning_lesson/data/model/SubLessonModel.kt
@@ -1,12 +1,30 @@
 package com.example.peil.ui.screens.learning_lesson.data.model
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
 data class SubLessonModel(
     val idSubLesson: Int,
     val header: String = "",
     val newWord: String = "",
     val translationWord: String = "",
-    val completed: Boolean,
+    var completed: MutableState<Boolean> = mutableStateOf(false),
     val options: Array<String> = arrayOf(""),
     val correctOption: String = "",
     val type: Int
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SubLessonModel
+
+        if (!options.contentEquals(other.options)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return options.contentHashCode()
+    }
+}
