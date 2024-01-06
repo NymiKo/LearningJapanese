8bb977727beafb38eca17fda4bff40add9afa0bb
NymiKo <dimon.kabernik8@gmail.com>
2024-01-06 21:02:01 +0500

    Changing the execution when the lesson is completed successfully

--- app/src/main/java/com/example/peil/ui/screens/learning_lesson/LearningLessonNavigation.kt
+++ app/src/main/java/com/example/peil/ui/screens/learning_lesson/LearningLessonNavigation.kt
@@ -1,5 +1,6 @@
 package com.example.peil.ui.screens.learning_lesson
 
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -8,7 +9,8 @@ const val learningLessonScreenRoute = "learning_lesson"
 
 fun NavGraphBuilder.learningLessonScreen() {
     composable(learningLessonScreenRoute) {
-        LearningLessonScreen()
+        val viewModel: LearningLessonViewModel = hiltViewModel()
+        LearningLessonScreen(viewModel)
     }
 }
 
--- app/src/main/java/com/example/peil/ui/screens/learning_lesson/LearningLessonScreen.kt
+++ app/src/main/java/com/example/peil/ui/screens/learning_lesson/LearningLessonScreen.kt
@@ -1,5 +1,7 @@
 package com.example.peil.ui.screens.learning_lesson
 
+import android.util.Log
+import android.util.LogPrinter
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,10 +19,17 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.peil.ui.screens.learning_lesson.data.model.SubLessonModel
 import com.example.peil.ui.screens.sublessons.choosing_option.SubLessonChoosingOptionItem
 import com.example.peil.ui.screens.sublessons.new_info.SubLessonNewInfoScreen
@@ -28,44 +37,24 @@ import com.example.peil.ui.theme.Blue
 import com.example.peil.ui.theme.GreyLight
 
 @Composable
-fun LearningLessonScreen() {
-
-    val lessonsList = listOf(
-        SubLessonModel(
-            idSubLesson = 0,
-            header = "Смотри-ка, кое-что новенькое!",
-            newWord = "Konnichiwa / こんにちは",
-            translationWord = "Здравствуйте",
-            completed = false,
-            type = 0
-        ),
-        SubLessonModel(
-            idSubLesson = 1,
-            header = "Выбери слово, которое переводится как: \"Привет\".",
-            newWord = "Konnichiwa / こんにちは",
-            translationWord = "Здравствуйте",
-            completed = false,
-            options = arrayOf("Konnichiwa", "Arigatou"),
-            correctOption = "Konnichiwa",
-            type = 1
-        )
-    )
-
+fun LearningLessonScreen(
+    viewModel: LearningLessonViewModel
+) {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        TopAppBar()
-        PagerLesson(lessonsList = lessonsList)
+        TopAppBar(progress = viewModel.progress)
+        PagerLesson(lessonsList = viewModel.subLessons, onCompleted = { item, completed -> viewModel.updateCompleted(item, completed) })
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun TopAppBar() {
+private fun TopAppBar(progress: Float) {
     TopAppBar(
         title = {
             LinearProgressIndicator(
-                progress = 0.2F,
+                progress = progress,
                 modifier = Modifier
                     .padding(horizontal = 16.dp)
                     .fillMaxWidth()
@@ -87,7 +76,7 @@ private fun TopAppBar() {
 }
 
 @Composable
-private fun PagerLesson(lessonsList: List<SubLessonModel>) {
+private fun PagerLesson(lessonsList: List<SubLessonModel>, onCompleted: (item: SubLessonModel, competed: Boolean) -> Unit) {
 
     val listState = rememberLazyListState()
 
@@ -99,9 +88,9 @@ private fun PagerLesson(lessonsList: List<SubLessonModel>) {
         itemsIndexed(lessonsList) {index, subLesson ->
             Box(modifier = Modifier.fillParentMaxSize()) {
                 when(subLesson.type) {
-                    0 -> SubLessonNewInfoScreen(subLesson, listState, index)
-                    1 -> SubLessonChoosingOptionItem(subLesson)
-                    else -> SubLessonNewInfoScreen(subLesson, listState, index)
+                    0 -> SubLessonNewInfoScreen(subLesson, listState, index, onCompleted = { completed -> onCompleted(lessonsList[index], completed) })
+                    1 -> SubLessonChoosingOptionItem(subLesson, onCompleted = { completed -> onCompleted(lessonsList[index], completed) })
+                    else -> SubLessonNewInfoScreen(subLesson, listState, index, onCompleted = { completed -> onCompleted(lessonsList[index], completed) })
                 }
             }
         }
@@ -111,5 +100,5 @@ private fun PagerLesson(lessonsList: List<SubLessonModel>) {
 @Preview
 @Composable
 private fun LearningLessonScreenPreview() {
-    LearningLessonScreen()
+    LearningLessonScreen(hiltViewModel())
 }
