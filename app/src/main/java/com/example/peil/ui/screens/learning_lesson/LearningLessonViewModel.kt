c50c5fce52de1a60fe27752d298c27301b4462f1
NymiKo <dimon.kabernik8@gmail.com>
2024-01-09 00:01:06 +0500

    Added sublesson objects to the list

--- app/src/main/java/com/example/peil/ui/screens/learning_lesson/LearningLessonViewModel.kt
+++ app/src/main/java/com/example/peil/ui/screens/learning_lesson/LearningLessonViewModel.kt
@@ -12,7 +12,7 @@ import javax.inject.Inject
 class LearningLessonViewModel @Inject constructor(): ViewModel() {
 
     private val _subLessons = listOf(
-            SubLessonModel(
+        SubLessonModel(
             idSubLesson = 0,
             header = "Смотри-ка, кое-что новенькое!",
             newWord = "Konnichiwa / こんにちは",
@@ -20,17 +20,46 @@ class LearningLessonViewModel @Inject constructor(): ViewModel() {
             completed = mutableStateOf(false),
             type = 0
         ),
-            SubLessonModel(
-                idSubLesson = 1,
-                header = "Выбери слово, которое переводится как: \"Привет\".",
-                newWord = "Konnichiwa / こんにちは",
-                translationWord = "Здравствуйте",
-                completed = mutableStateOf(false),
-                options = arrayOf("Konnichiwa", "Arigatou"),
-                correctOption = "Konnichiwa",
-                type = 1
-            )
-        ).toMutableStateList()
+        SubLessonModel(
+            idSubLesson = 1,
+            header = "Выбери слово, которое переводится как: \"Привет\".",
+            newWord = "",
+            translationWord = "Здравствуйте",
+            completed = mutableStateOf(false),
+            options = arrayOf("Konnichiwa", "Arigatou"),
+            correctOption = "Konnichiwa",
+            type = 1
+        ),
+        SubLessonModel(
+            idSubLesson = 2,
+            header = "Смотри-ка, кое-что новенькое!",
+            newWord = "Hajimemashite / はじめまして",
+            translationWord = "Приятно познакомиться",
+            completed = mutableStateOf(false),
+            type = 0
+        ),
+        SubLessonModel(
+            idSubLesson = 3,
+            header = "Верно или неверно?",
+            newWord = "\"Hajimemashite\" означает \"приятно познакомиться\".",
+            translationWord = "",
+            completed = mutableStateOf(false),
+            options = arrayOf("Правильно", "Неправильно"),
+            correctOption = "Правильно",
+            remark = "\"Hajimemashite\" говорят, когда встречают кого-то впервые",
+            type = 2
+        ),
+        SubLessonModel(
+            idSubLesson = 4,
+            header = "Выбери предложение, чтобы сказать: \"Приятно познакомиться\".",
+            newWord = "",
+            translationWord = "Приятно познакомиться",
+            completed = mutableStateOf(false),
+            options = arrayOf("Konnichiwa", "Hajimemashite"),
+            correctOption = "Hajimemashite",
+            type = 1
+        )
+    ).toMutableStateList()
 
     val subLessons: List<SubLessonModel> get() = _subLessons
 
