4ec2fb54aaadbe4e71a8a300301e04c5bc20e47d
NymiKo <dimon.kabernik8@gmail.com>
2024-01-09 15:15:50 +0500

    Added the image element from the glide library to display the image by link

--- app/src/main/java/com/example/peil/ui/screens/sublessons/new_info/SubLessonNewInfoItem.kt
+++ app/src/main/java/com/example/peil/ui/screens/sublessons/new_info/SubLessonNewInfoItem.kt
@@ -1,21 +1,27 @@
 package com.example.peil.ui.screens.sublessons.new_info
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.peil.R
 import com.example.peil.ui.screens.learning_lesson.data.model.SubLessonModel
 import com.example.peil.ui.theme.GreyLight
@@ -24,6 +30,7 @@ import com.example.peil.ui.theme.GreyLightBD
 import com.example.peil.ui.view_components.LoginButton
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun SubLessonNewInfoScreen(
     subLessonItem: SubLessonModel,
@@ -37,6 +44,14 @@ fun SubLessonNewInfoScreen(
             .fillMaxSize()
     ) {
         HeaderLesson(headerText = subLessonItem.header)
+        GlideImage(
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .border(width = 1.dp, color = GreyLightBD, shape = RoundedCornerShape(10.dp)),
+            model = subLessonItem.lessonImage,
+            contentDescription = null
+        )
         NewWordLesson(newWordText = subLessonItem.newWord)
         TranslationWordLesson(translationWordText = subLessonItem.translationWord)
         Spacer(modifier = Modifier.weight(1f))
