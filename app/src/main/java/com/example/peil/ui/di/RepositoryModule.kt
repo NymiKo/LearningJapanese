a1073fc875257e42fcc5050e926b5129233d9382
NymiKo <dimon.kabernik8@gmail.com>
2024-01-12 15:35:03 +0500

    LearningLessonRepository added to the hilt dependency graph

--- app/src/main/java/com/example/peil/ui/di/RepositoryModule.kt
+++ app/src/main/java/com/example/peil/ui/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.example.peil.ui.di
 
 import com.example.peil.ui.screens.create_account.data.CreateAccountRepository
 import com.example.peil.ui.screens.create_account.data.CreateAccountRepositoryImpl
+import com.example.peil.ui.screens.learning_lesson.data.LearningLessonRepository
+import com.example.peil.ui.screens.learning_lesson.data.LearningLessonRepositoryImpl
 import com.example.peil.ui.screens.lessons_list.data.LessonsListRepository
 import com.example.peil.ui.screens.lessons_list.data.LessonsListRepositoryImpl
 import com.example.peil.ui.screens.login.data.LoginRepository
@@ -27,4 +29,8 @@ interface RepositoryModule {
     @Binds
     @Singleton
     fun bindLessonsListRepository(repository: LessonsListRepositoryImpl): LessonsListRepository
+
+    @Binds
+    @Singleton
+    fun bindLearningLessonRepository(repository: LearningLessonRepositoryImpl): LearningLessonRepository
 }
