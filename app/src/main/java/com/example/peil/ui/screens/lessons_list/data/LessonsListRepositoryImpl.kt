e74ba43ee4d6dc81416105d67817b5b0f168b250
NymiKo <dimon.kabernik8@gmail.com>
2024-01-12 22:30:07 +0500

    The conversion of the list of lessons from the server to the list of lessons in the application has been changed

--- app/src/main/java/com/example/peil/ui/screens/lessons_list/data/LessonsListRepositoryImpl.kt
+++ app/src/main/java/com/example/peil/ui/screens/lessons_list/data/LessonsListRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.peil.ui.screens.lessons_list.data
 
+import android.util.Log
 import com.example.peil.data.NetworkResult
 import com.example.peil.data.handleApi
 import com.example.peil.ui.screens.lessons_list.data.model.LessonModel
@@ -10,15 +11,26 @@ import javax.inject.Inject
 class LessonsListRepositoryImpl @Inject constructor(
     private val ioDispatcher: CoroutineDispatcher,
     private val lessonsListService: LessonsListService
-): LessonsListRepository {
-    override suspend fun getLessonsList(): NetworkResult<Map<String, List<LessonModel>>> = withContext(ioDispatcher) {
-        return@withContext when(val result = handleApi { lessonsListService.getLessonsList() }) {
-            is NetworkResult.Error -> {
-                NetworkResult.Error(result.code)
-            }
-            is NetworkResult.Success -> {
-                NetworkResult.Success(result.data.map { it.toLessonModel() }.groupBy { it.chapter }.toSortedMap())
+) : LessonsListRepository {
+    override suspend fun getLessonsList(): NetworkResult<Map<Int, List<LessonModel>>> =
+        withContext(ioDispatcher) {
+            return@withContext when (val result =
+                handleApi { lessonsListService.getLessonsList() }) {
+                is NetworkResult.Error -> {
+                    NetworkResult.Error(result.code)
+                }
+
+                is NetworkResult.Success -> {
+                    NetworkResult.Success(result.data.lessonsList.map {
+                        LessonModel(
+                            it.idLesson,
+                            it.header,
+                            it.image,
+                            it.chapter,
+                            result.data.lessonsCompletedList.contains(it.idLesson)
+                        )
+                    }.groupBy { it.chapter }.toSortedMap())
+                }
             }
         }
-    }
 }
--- app/src/main/java/com/example/peil/ui/screens/lessons_list/data/LessonsListService.kt
+++ app/src/main/java/com/example/peil/ui/screens/lessons_list/data/LessonsListService.kt
@@ -1,10 +1,11 @@
 package com.example.peil.ui.screens.lessons_list.data
 
+import com.example.peil.ui.screens.lessons_list.data.model.LessonCompletedResponse
 import com.example.peil.ui.screens.lessons_list.data.model.LessonResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface LessonsListService {
     @GET("getLessonsList.php")
-    suspend fun getLessonsList(): Response<List<LessonResponse>>
+    suspend fun getLessonsList(): Response<LessonCompletedResponse>
 }
