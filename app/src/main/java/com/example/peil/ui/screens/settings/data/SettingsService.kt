f5bb33c24078a02a16a33ba89d902bd11407aba0
NymiKo <dimon.kabernik8@gmail.com>
2024-01-25 17:50:55 +0500

    A method has been added to SettingsService to get profile data

--- app/src/main/java/com/example/peil/ui/screens/settings/data/SettingsService.kt
+++ app/src/main/java/com/example/peil/ui/screens/settings/data/SettingsService.kt
@@ -1,5 +1,6 @@
 package com.example.peil.ui.screens.settings.data
 
+import com.example.peil.ui.screens.profile.data.model.ProfileResponseModel
 import okhttp3.MultipartBody
 import retrofit2.Response
 import retrofit2.http.Multipart
@@ -10,4 +11,7 @@ interface SettingsService {
     @Multipart
     @POST("loadAvatar.php")
     suspend fun loadAvatar(@Part avatar: MultipartBody.Part): Response<Unit>
+
+    @POST("getFullProfile.php")
+    suspend fun getFullProfile(): Response<ProfileResponseModel>
 }
