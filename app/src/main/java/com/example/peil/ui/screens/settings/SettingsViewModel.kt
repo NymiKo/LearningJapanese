51f80a1a945d6580ac99621a3887a9f5df07e1d1
NymiKo <dimon.kabernik8@gmail.com>
2024-01-25 17:52:38 +0500

    A method has been added to the SettingsViewModel to handle the ui state for profile data

--- app/src/main/java/com/example/peil/ui/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/peil/ui/screens/settings/SettingsViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.peil.ui.screens.settings
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.peil.data.NetworkResult
+import com.example.peil.ui.screens.profile.data.model.ProfileModel
 import com.example.peil.ui.screens.settings.data.SettingsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,8 +17,25 @@ class SettingsViewModel @Inject constructor(
     private val repository: SettingsRepository
 ): ViewModel() {
 
+    private val _profile = MutableLiveData<ProfileModel>()
+    val profile: LiveData<ProfileModel> = _profile
+
+    init {
+        getFullProfile()
+    }
+
+    private fun getFullProfile() = viewModelScope.launch {
+        when(val result = repository.getFullProfile()) {
+            is NetworkResult.Error -> {
+
+            }
+            is NetworkResult.Success -> {
+                _profile.value = result.data
+            }
+        }
+    }
+
     fun loadAvatar(file: File) = viewModelScope.launch {
         repository.loadAvatar(file)
     }
-
 }
