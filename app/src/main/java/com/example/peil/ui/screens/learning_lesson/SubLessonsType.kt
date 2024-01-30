f61976d3ed28a529348b19c48c1df218aa09ede8
NymiKo <dimon.kabernik8@gmail.com>
2024-01-30 21:25:08 +0500

    Added enum class for sublesson types

---
+++ app/src/main/java/com/example/peil/ui/screens/learning_lesson/SubLessonsType.kt
@@ -0,0 +1,10 @@
+package com.example.peil.ui.screens.learning_lesson
+
+enum class SubLessonsType(val type: Int) {
+    SubLessonNewInfo(0),
+    SubLessonChoosingOption(1),
+    SubLessonTrueOrFalse(2),
+    SubLessonFinishSentenceItem(3),
+    SubLessonArrangeWordsItem(4),
+    SubLessonHintItem(5)
+}
