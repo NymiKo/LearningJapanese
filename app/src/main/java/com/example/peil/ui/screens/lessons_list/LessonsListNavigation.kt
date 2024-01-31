0f2dbe5327bb6d43742d448c7f02414e3cbf4be8
NymiKo <dimon.kabernik8@gmail.com>
2024-01-31 14:45:47 +0500

    Added animation of the transition from the profile to the list of lessons

--- app/src/main/java/com/example/peil/ui/screens/lessons_list/LessonsListNavigation.kt
+++ app/src/main/java/com/example/peil/ui/screens/lessons_list/LessonsListNavigation.kt
@@ -1,16 +1,40 @@
 package com.example.peil.ui.screens.lessons_list
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.example.peil.ui.screens.profile.profileScreenRoute
 
 const val lessonsListScreenRoute = "lessons_list"
 
 fun NavGraphBuilder.lessonsListScreen(
     onLearningLesson: (idLesson: Int) -> Unit
 ) {
-    composable(lessonsListScreenRoute) {
+    composable(
+        route = lessonsListScreenRoute,
+        enterTransition = {
+            when(initialState.destination.route) {
+                profileScreenRoute -> {
+                    slideInHorizontally(
+                        initialOffsetX = { -it },
+                        animationSpec = tween(300)
+                    )
+                }
+
+                else -> null
+            }
+        },
+        exitTransition = {
+            slideOutHorizontally(
+                targetOffsetX = { -it },
+                animationSpec = tween(300)
+            )
+        }
+    ) {
         val viewModel: LessonsListViewModel = hiltViewModel()
         LessonsListScreen(
             onLearningLesson = onLearningLesson::invoke,
