e5dff50797c35bd4694476388a7d146ad1476a75
NymiKo <dimon.kabernik8@gmail.com>
2024-01-31 14:44:43 +0500

    Added audio player creation and audio playback display

---
+++ app/src/main/java/com/example/peil/ui/view_components/AudioPlayer.kt
@@ -0,0 +1,124 @@
+package com.example.peil.ui.view_components
+
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.peil.ui.theme.baseBlue
+import kotlinx.coroutines.delay
+
+@Composable
+fun AudioPlayer(audio: String) {
+
+    var mediaPlayer by remember { mutableStateOf<MediaPlayer?>(null) }
+    var isPlaying by remember { mutableStateOf(false) }
+    var currentPosition by remember { mutableStateOf(0) }
+    var totalDuration by remember { mutableStateOf(0) }
+
+    DisposableEffect(Unit) {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer()
+            mediaPlayer!!.setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build()
+            )
+            mediaPlayer!!.setDataSource(audio)
+            mediaPlayer!!.prepare()
+            totalDuration = mediaPlayer!!.duration
+        }
+
+        onDispose {
+            mediaPlayer?.release()
+        }
+    }
+
+    LaunchedEffect(isPlaying) {
+        if (mediaPlayer?.isPlaying == true && !isPlaying) {
+            mediaPlayer?.pause()
+        } else if (isPlaying) {
+            mediaPlayer?.start()
+        }
+    }
+
+    LaunchedEffect(isPlaying) {
+        while (isPlaying && currentPosition < totalDuration) {
+            currentPosition = mediaPlayer?.currentPosition ?: 0
+            delay(100)
+        }
+
+        if (currentPosition >= totalDuration) {
+            currentPosition = 0
+            isPlaying = false
+        }
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(baseBlue, RoundedCornerShape(8.dp)),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(
+            onClick = { isPlaying = !isPlaying }
+        ) {
+            Icon(
+                modifier = Modifier.size(35.dp),
+                imageVector = if (isPlaying) Icons.Default.Pause else Icons.Default.PlayArrow,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+
+        Slider(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .weight(1F),
+            value = currentPosition.toFloat(),
+            onValueChange = {
+                currentPosition = it.toInt()
+                mediaPlayer?.seekTo(currentPosition)
+            },
+            valueRange = 0F..totalDuration.toFloat()
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun AudioPlayerPreview() {
+    Surface {
+        AudioPlayer("")
+    }
+}
