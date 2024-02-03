f7aa190428212337ec01fc4e12a13c54fd78018f
NymiKo <dimon.kabernik8@gmail.com>
2024-02-03 22:10:21 +0500

    Added a method for getting audio from the server for download

--- app/src/main/java/com/example/peil/ui/screens/learning_lesson/data/LearningLessonService.kt
+++ app/src/main/java/com/example/peil/ui/screens/learning_lesson/data/LearningLessonService.kt
@@ -1,11 +1,18 @@
 package com.example.peil.ui.screens.learning_lesson.data
 
 import com.example.peil.ui.screens.learning_lesson.data.model.SubLessonResponseModel
+import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Streaming
+import retrofit2.http.Url
 
 interface LearningLessonService {
     @GET("getSubLessons.php")
     suspend fun getSubLesson(@Query("idLesson") idLesson: Int): Response<List<SubLessonResponseModel>>
+
+    @Streaming
+    @GET
+    suspend fun downloadAudio(@Url fileUrl: String): Response<ResponseBody>
 }
