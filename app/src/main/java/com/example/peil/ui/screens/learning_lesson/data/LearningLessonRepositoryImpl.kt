3e46b637e3901f81daac38f0037efc2b7b016ac3
NymiKo <dimon.kabernik8@gmail.com>
2024-02-03 22:11:20 +0500

    Sublesson data download has been updated First, data is received from the local database, and then from the server

--- app/src/main/java/com/example/peil/ui/screens/learning_lesson/data/LearningLessonRepositoryImpl.kt
+++ app/src/main/java/com/example/peil/ui/screens/learning_lesson/data/LearningLessonRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.peil.ui.screens.learning_lesson.data
 
 import com.example.peil.data.NetworkResult
 import com.example.peil.data.handleApi
+import com.example.peil.data.room.dao.SubLessonDao
 import com.example.peil.ui.screens.learning_lesson.data.model.SubLessonModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
@@ -9,16 +10,22 @@ import javax.inject.Inject
 
 class LearningLessonRepositoryImpl @Inject constructor(
     private val ioDispatcher: CoroutineDispatcher,
-    private val learningLessonService: LearningLessonService
+    private val learningLessonService: LearningLessonService,
+    private val subLessonDao: SubLessonDao
 ): LearningLessonRepository {
     override suspend fun getSubLessons(idLesson: Int): NetworkResult<List<SubLessonModel>> = withContext(ioDispatcher) {
-        return@withContext when(val result = handleApi { learningLessonService.getSubLesson(idLesson) }) {
-            is NetworkResult.Error -> {
-                NetworkResult.Error(result.code)
+        val subLessonsList = subLessonDao.getSubLessons(idLesson)
+        if (subLessonsList.isEmpty()) {
+            return@withContext when(val result = handleApi { learningLessonService.getSubLesson(idLesson) }) {
+                is NetworkResult.Error -> {
+                    NetworkResult.Error(result.code)
+                }
+                is NetworkResult.Success -> {
+                    NetworkResult.Success(result.data.map { it.toSubLessonModel() })
+                }
             }
-            is NetworkResult.Success -> {
-                NetworkResult.Success(result.data.map { it.toSubLessonModel() })
-            }
+        } else {
+            NetworkResult.Success(subLessonsList.map { it.toSubLessonModel() })
         }
     }
 }
