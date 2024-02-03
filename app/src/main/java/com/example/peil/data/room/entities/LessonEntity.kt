0398422880e473d78c85641ca06e01d8137a303c
NymiKo <dimon.kabernik8@gmail.com>
2024-02-03 19:33:11 +0500

    Added an entity for a table with a list of lessons

---
+++ app/src/main/java/com/example/peil/data/room/entities/LessonEntity.kt
@@ -0,0 +1,36 @@
+package com.example.peil.data.room.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.peil.data.room.RoomContract
+import com.example.peil.ui.screens.lessons_list.data.model.LessonModel
+
+@Entity(
+    tableName = RoomContract.tableLessons
+)
+data class LessonEntity(
+    @PrimaryKey(autoGenerate = false)
+    val idLesson: Int,
+    val header: String,
+    val image: String,
+    val chapter: Int,
+    val completed: Boolean = false
+) {
+    companion object {
+        fun toLessonEntity(lesson: LessonModel) = LessonEntity(
+            idLesson = lesson.idLesson,
+            header = lesson.header,
+            image = lesson.image,
+            chapter = lesson.chapter,
+            completed = lesson.completed
+        )
+    }
+
+    fun toLessonModel() = LessonModel(
+        idLesson = idLesson,
+        header = header,
+        image = image,
+        chapter = chapter,
+        completed =completed
+    )
+}
