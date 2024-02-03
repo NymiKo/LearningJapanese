7e06fbc7748854878935576deac7ef533ca13031
NymiKo <dimon.kabernik8@gmail.com>
2024-02-03 19:44:51 +0500

    Database and table creation added to the hilt dependency graph

---
+++ app/src/main/java/com/example/peil/ui/di/RoomModule.kt
@@ -0,0 +1,25 @@
+package com.example.peil.ui.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.peil.data.room.RoomContract
+import com.example.peil.data.room.RoomDatabaseApp
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomModule {
+
+    @Singleton
+    @Provides
+    fun provideRoom(@ApplicationContext context: Context): RoomDatabaseApp =
+        Room.databaseBuilder(context, RoomDatabaseApp::class.java, RoomContract.databaseApp).build()
+
+    @Provides
+    fun provideLessonDao(database: RoomDatabaseApp) = database.lessonDao()
+}
