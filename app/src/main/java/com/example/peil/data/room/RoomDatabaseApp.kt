4ac06b374b8998b8bee4b117aa41e63d2cb7a56a
NymiKo <dimon.kabernik8@gmail.com>
2024-02-03 22:11:56 +0500

    Updating the lesson execution in the local database

--- app/src/main/java/com/example/peil/data/room/RoomDatabaseApp.kt
+++ app/src/main/java/com/example/peil/data/room/RoomDatabaseApp.kt
@@ -3,15 +3,19 @@ package com.example.peil.data.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.peil.data.room.dao.LessonDao
+import com.example.peil.data.room.dao.SubLessonDao
 import com.example.peil.data.room.entities.LessonEntity
+import com.example.peil.data.room.entities.SubLessonEntity
 
 @Database(
     entities = [
         LessonEntity::class,
+        SubLessonEntity::class
     ],
     version = 1,
     exportSchema = true
 )
 abstract class RoomDatabaseApp : RoomDatabase() {
     abstract fun lessonDao(): LessonDao
+    abstract fun subLessonDao(): SubLessonDao
 }
--- app/src/main/java/com/example/peil/ui/screens/lesson_completion/data/LessonCompletionRepositoryImpl.kt
+++ app/src/main/java/com/example/peil/ui/screens/lesson_completion/data/LessonCompletionRepositoryImpl.kt
@@ -2,15 +2,19 @@ package com.example.peil.ui.screens.lesson_completion.data
 
 import com.example.peil.data.NetworkResult
 import com.example.peil.data.handleApi
+import com.example.peil.data.room.dao.LessonDao
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class LessonCompletionRepositoryImpl @Inject constructor(
     private val ioDispatcher: CoroutineDispatcher,
-    private val lessonCompletionService: LessonCompletionService
-): LessonCompletionRepository {
-    override suspend fun lessonCompleted(idLesson: Int): NetworkResult<Unit> = withContext(ioDispatcher) {
-        return@withContext handleApi { lessonCompletionService.lessonCompleted(idLesson) }
-    }
+    private val lessonCompletionService: LessonCompletionService,
+    private val lessonDao: LessonDao
+) : LessonCompletionRepository {
+    override suspend fun lessonCompleted(idLesson: Int): NetworkResult<Unit> =
+        withContext(ioDispatcher) {
+            lessonDao.updateLesson(idLesson, true)
+            return@withContext handleApi { lessonCompletionService.lessonCompleted(idLesson) }
+        }
 }
