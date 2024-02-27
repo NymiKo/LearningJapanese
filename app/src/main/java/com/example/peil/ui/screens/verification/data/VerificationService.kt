281afff488532f13e35bac41c3cebfe32f696e92
NymiKo <dimon.kabernik8@gmail.com>
2024-02-27 21:04:45 +0500

    Added a service for sending account data to create it

---
+++ app/src/main/java/com/example/peil/ui/screens/verification/data/VerificationService.kt
@@ -0,0 +1,12 @@
+package com.example.peil.ui.screens.verification.data
+
+import com.example.peil.ui.screens.verification.data.model.VerificationRequest
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+
+interface VerificationService {
+    @POST("verification.php")
+    suspend fun verificationUser(@Body verificationRequest: VerificationRequest): Response<String>
+}
---
+++ app/src/main/java/com/example/peil/ui/screens/verification/data/model/VerificationRequest.kt
@@ -0,0 +1,6 @@
+package com.example.peil.ui.screens.verification.data.model
+
+data class VerificationRequest(
+    val idUser: Int,
+    val code: String
+)
