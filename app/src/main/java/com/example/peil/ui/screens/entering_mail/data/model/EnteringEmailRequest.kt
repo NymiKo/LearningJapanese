e037834d0f82b38c041cf950ba4b25af9ff32ded
NymiKo <dimon.kabernik8@gmail.com>
2024-02-29 02:12:15 +0500

    Added a navigation file for the EnteringEmail screen

---
+++ app/src/main/java/com/example/peil/ui/screens/entering_mail/data/model/EnteringEmailRequest.kt
@@ -0,0 +1,5 @@
+package com.example.peil.ui.screens.entering_mail.data.model
+
+data class EnteringEmailRequest(
+    val email: String
+)
---
+++ app/src/main/java/com/example/peil/ui/screens/entering_mail/navigation/EnteringEmailScreenNavigation.kt
@@ -0,0 +1,32 @@
+package com.example.peil.ui.screens.entering_mail.navigation
+
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.peil.ui.screens.entering_mail.EnteringEmailScreen
+import com.example.peil.ui.screens.entering_mail.EnteringEmailViewModel
+
+const val enteringEmailScreenRoute = "entering_email"
+
+fun NavGraphBuilder.enteringEmail() {
+    composable(
+        route = enteringEmailScreenRoute,
+        enterTransition = {
+            slideInHorizontally(initialOffsetX = { it }, animationSpec = tween(300))
+        },
+        popExitTransition = {
+            slideOutHorizontally(targetOffsetX = { it }, animationSpec = tween(300))
+        }
+    ) {
+        val viewModel: EnteringEmailViewModel = hiltViewModel()
+        EnteringEmailScreen(viewModel = viewModel)
+    }
+}
+
+fun NavController.navigateToEnteringEmail() {
+    navigate(enteringEmailScreenRoute)
+}
